2e81e003a9d5fd0bbad713521a70c14257093a0a
Şahin EKMEN <sahin.ekmen6868@gmail.com>
2024-07-10 14:58:15 +0300

    AsenkronMultiThread
    Kendimi  geliştirmek için AsenkronMultiThread üzerinde Yaptığım Mantığını Anlamak için  yaptığım  uygulama

---
+++ AsenkronMultiThread/AsenkronMultiThread/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace AsenkronMultiThread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReadFile = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.BtnCounter = new System.Windows.Forms.Button();
+            this.textBoxCounter = new System.Windows.Forms.TextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnReadFile
+            // 
+            this.btnReadFile.Location = new System.Drawing.Point(42, 12);
+            this.btnReadFile.Name = "btnReadFile";
+            this.btnReadFile.Size = new System.Drawing.Size(165, 46);
+            this.btnReadFile.TabIndex = 0;
+            this.btnReadFile.Text = "Dosya Oku";
+            this.btnReadFile.UseVisualStyleBackColor = true;
+            this.btnReadFile.Click += new System.EventHandler(this.btnReadFile_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(42, 79);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(165, 202);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // BtnCounter
+            // 
+            this.BtnCounter.Location = new System.Drawing.Point(372, 12);
+            this.BtnCounter.Name = "BtnCounter";
+            this.BtnCounter.Size = new System.Drawing.Size(158, 46);
+            this.BtnCounter.TabIndex = 2;
+            this.BtnCounter.Text = "SayaçArtır";
+            this.BtnCounter.UseVisualStyleBackColor = true;
+            this.BtnCounter.Click += new System.EventHandler(this.BtnCounter_Click);
+            // 
+            // textBoxCounter
+            // 
+            this.textBoxCounter.Location = new System.Drawing.Point(398, 64);
+            this.textBoxCounter.Name = "textBoxCounter";
+            this.textBoxCounter.Size = new System.Drawing.Size(100, 22);
+            this.textBoxCounter.TabIndex = 3;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(372, 92);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(173, 189);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.textBoxCounter);
+            this.Controls.Add(this.BtnCounter);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnReadFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReadFile;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button BtnCounter;
+        private System.Windows.Forms.TextBox textBoxCounter;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ AsenkronMultiThread/AsenkronMultiThread/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsenkronMultiThread
+{
+    public partial class Form1 : Form
+    {
+
+        public int counter { get; set; } = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnReadFile_Click(object sender, EventArgs e)
+        {
+            String data = string.Empty;
+            Task<string> okuma =ReadFileAsync();
+
+            //   string data =await  ReadFileAsync();
+      
+
+            richTextBox2.Text = await new HttpClient().GetStringAsync("https://www.google.com.tr/");
+            data = await okuma;
+
+            richTextBox1.Text = data;
+        }
+
+        private void BtnCounter_Click(object sender, EventArgs e)
+        {
+            textBoxCounter.Text = counter++.ToString();
+        }
+
+
+
+        private string ReadFile()
+        {
+            string data = string.Empty;
+
+            using (StreamReader s = new StreamReader("Yeni Metin Belgesi.txt"))
+            {
+
+                Thread.Sleep(5000);
+                data = s.ReadToEnd();
+
+            }
+            return data;
+
+        }
+        // void task
+        // string task<string>
+
+        private async Task<string> ReadFileAsync()
+        {
+
+            string data=string.Empty;
+            using (StreamReader s=new StreamReader("Yeni Metin Belgesi.txt"))
+            {
+                 Task<string> myTask = s.ReadToEndAsync();
+
+               await Task.Delay(5000);
+
+
+                data = await myTask;
+
+                    return data;
+            }
+            
+        }
+
+
+
+    }
+}
